1bfbea1702415fe18f6fa0165f2db9432342360b
John Victor Dato-on <143370209+johnvictordatoon@users.noreply.github.com>
2025-02-18 17:50:15 +0800

    added the needed requirements

--- word frequency counter php/index.php
+++ word frequency counter php/index.php
@@ -1,9 +1,22 @@
 <?php
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $inputText = $_POST['text'];
-    $sortingOrder = $_POST['sorting_order']; 
-    $displayLimit = $_POST['display_limit'];
+    $sortingOrder = $_POST['sort'];
+    $displayLimit = $_POST['limit'];
+
+    $stopwords_common = array("the", "and", "in", "of", "to", "a", "is", "that", "it", "with", "as", "for", "was", "on", "at", "by", "an");
+    $words = str_word_count(strtolower($inputText), 1);
+    $filtered_words = array_diff($words, $stopwords_common);
+    $word_freq = array_count_values($filtered_words);
+
+    if ($sortingOrder === 'asc') {
+        asort($word_freq);
     }
+    else {
+        arsort($word_freq);
+    }
+
+    $word_freq = array_slice($word_freq, 0, $displayLimit, true);
 }
 ?>
 
@@ -13,12 +26,11 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     <meta charset="UTF-8">
     <title>Word Frequency Counter</title>
     <link rel="stylesheet" type="text/css" href="styles.css">
-
 </head>
 <body>
     <h1>Word Frequency Counter</h1>
     
-    <form action="process.php" method="post">
+    <form action="index.php" method="post">
         <label for="text">Paste your text here:</label><br>
         <textarea id="text" name="text" rows="10" cols="50" required></textarea><br><br>
         
@@ -29,9 +41,18 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
         </select><br><br>
         
         <label for="limit">Number of words to display:</label>
-        <input type="number" id="limit" name="limit" value="10" min="1"><br><br>
+        <input type="number" id="limit" name="limit" min="1" placeholder="ex: 5" required><br><br>
         
-        <input type="submit" value="Calculate Word Frequency">
+        <input type="submit" value="Submit">
     </form>
+
+    <?php if ($_SERVER['REQUEST_METHOD'] === 'POST'): ?>
+        <h2>Counted Words (Frequency)</h2>
+        <ul>
+            <?php foreach ($word_freq as $word => $frequency): ?>
+                <li><?php echo htmlspecialchars($word) . ': ' . $frequency; ?></li>
+            <?php endforeach; ?>
+        </ul>
+    <?php endif; ?>
 </body>
 </html>
